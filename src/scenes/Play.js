7298f66834b9f4e6f9ae28bd1424d5dc254d3e78
nktwang <nktwang@gmail.com>
2023-04-28 08:10:24 -0700

    fifth commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,7 +10,10 @@ class Play extends Phaser.Scene {
         this.load.image('starfield', './assets/starfield.png');
       }
     create() {
-      // green UI background
+        update() {
+            this.starfield.tilePositionX -= 4;
+          }  
+        // green UI background
       console.log(borderUISize)
       // place tile sprite
       this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
@@ -20,5 +23,12 @@ class Play extends Phaser.Scene {
       this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+      // add rocket (p1)
+      this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+      // define keys
+      keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+      keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+      keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+      keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
   }
