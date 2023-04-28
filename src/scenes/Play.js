b5ddfc97e54b9d2f543f7f0f64c598d436eefbc2
nktwang <nktwang@gmail.com>
2023-04-28 07:32:15 -0700

    Fourth Commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,8 +2,23 @@ class Play extends Phaser.Scene {
     constructor() {
       super("playScene");
     }
-    
+
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+      }
     create() {
-      this.add.text(20, 20, "Rocket Patrol Menu");
+      // green UI background
+      console.log(borderUISize)
+      // place tile sprite
+      this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+      this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+      // white borders
+      this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+      this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+      this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+      this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
   }
