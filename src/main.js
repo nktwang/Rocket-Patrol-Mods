d18293f2f6e624cdd8337afb4e940267dda1cbc6
nktwang <nktwang@gmail.com>
2023-06-08 19:57:50 -0700

    Fourth Commit: Modified Rocket.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+//Noctis Wang
+//Rocket Patrol Mods
+//1h30m
+//Track a high score that persists across scenes and display it in the UI (5)
+//user3289402, et al. “Create a High Score in Phaser.” Stack Overflow, 1 Feb. 1963, https://stackoverflow.com/questions/37408825/create-a-high-score-in-phaser. 
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
