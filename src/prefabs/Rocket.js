ffd77b22bbaf774b696de34f3ffd5b92d0a44bea
nktwang <nktwang@gmail.com>
2023-06-08 20:00:14 -0700

    Fourth Commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -9,33 +9,47 @@ class Rocket extends Phaser.GameObjects.Sprite {
         this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
     }
 
-    update() {
-        // left/right movement
-        if(!this.isFiring) {
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed;
-            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
-                this.x += this.moveSpeed;
+    update(){
+        /* if(!this.isFiring){
+            if(keyLEFT.isDown && this.x>=borderUISize+this.width){
+                this.x-=this.moveSpeed;
+            }else if(keyRIGHT.isDown && this.x<=game.config.width-borderUISize-this.width){
+                this.x+=this.moveSpeed;
             }
+        } */
+        // Allow the player to control the Rocket after it's fired (5)
+        if(keyLEFT.isDown && this.x>=borderUISize+this.width){
+            this.x-=this.moveSpeed;
+        }else if(keyRIGHT.isDown && this.x<=game.config.width-borderUISize-this.width){
+            this.x+=this.moveSpeed;
         }
-        // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
-            this.isFiring = true;
-            this.sfxRocket.play();
+
+        if(Phaser.Input.Keyboard.JustDown(keyF)&& !this.isFiring){
+            this.isFiring=true;
+            this.sfxRocket.play();  // play sfx
         }
-        // if fired, move up
-        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
-            this.y -= this.moveSpeed;
+        if(this.isFiring&&this.y>=borderUISize*3+borderPadding){
+            this.y -=this.moveSpeed;
         }
-        // reset on miss
-        if(this.y <= borderUISize * 3 + borderPadding) {
+        if(this.y<=borderUISize*3+borderPadding){
             this.reset();
         }
+        if(game.input.mousePointer.x<=borderUISize && (game.input.mousePointer.y>=(borderPadding+borderUISize)) && (game.input.mousePointer.y<=(game.config.height-borderPadding))){
+            this.x=borderUISize;
+        }else if(game.input.mousePointer.x>=game.config.width-borderUISize && (game.input.mousePointer.y>=(borderPadding+borderUISize)) && (game.input.mousePointer.y<=(game.config.height-borderPadding))){
+            this.x=game.config.width-borderUISize;
+        }else if((game.input.mousePointer.y>=(borderPadding+borderUISize)) && (game.input.mousePointer.y<=(game.config.height-borderPadding-borderUISize))){
+            this.x=game.input.mousePointer.x;
+        }
     }
-
-    // reset rocket to "ground"
-    reset() {
-        this.isFiring = false;
-        this.y = game.config.height - borderUISize - borderPadding;
+    reset(){
+        this.isFiring=false;
+        this.y=game.config.height-borderUISize-borderPadding;
+    }
+    shootOnClick(){
+        if(!this.isFiring){
+            this.isFiring=true;
+            this.sfxRocket.play();  // play sfx
+        }
     }
 }
