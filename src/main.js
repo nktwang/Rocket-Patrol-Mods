0f83479aa20de8da115b3394d9dfdcff92f587b6
nktwang <nktwang@gmail.com>
2023-06-13 17:26:31 -0700

    Commit 21: changed the description in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 //Noctis Wang
 //Rocket Patrol Mods
-//1h30m
+//3h30m
 //Track a high score that persists across scenes and display it in the UI (5)
 //Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
 //Allow the player to control the Rocket after it's fired (5)
+//Implement mouse control for player movement and mouse click to fire (15)
 //Create a new scrolling tile sprite for the background (5)
 //user3289402, et al. “Create a High Score in Phaser.” Stack Overflow, 1 Feb. 1963, https://stackoverflow.com/questions/37408825/create-a-high-score-in-phaser. 
 
